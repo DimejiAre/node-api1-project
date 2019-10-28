6b516877cafa2f4c4cc8fead00b70f6b09dcf596
DimejiAre <olardi2@gmail.com>
2019-10-28 13:07:23 +0100

    initialize server with endpoint for getting users

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// implement your API here
+const express = require('express');
+const db = require('./data/db');
+
+const app = express();
+
+app.get('/api/users', (req, res) => {
+    db.find()
+        .then(data => {
+            if(data){
+                res.status(200).json(data)
+            } else {
+                res.status(404).json({
+                    error: "No users exist"
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: "There was an error while saving the user to the database"
+            })
+        })
+});
+
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log('server listening on ' + (process.env.PORT || 3000))
+});
