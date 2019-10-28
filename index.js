604e721f4f0a7b8240be6087854ef6bb223c1f02
DimejiAre <olardi2@gmail.com>
2019-10-28 13:15:39 +0100

    add endpoint for getting users by ID

--- index.js
+++ index.js
@@ -21,6 +21,25 @@ app.get('/api/users', (req, res) => {
         })
 });
 
+app.get('/api/users/:id', (req, res) => {
+    const id = req.params.id
+    db.findById(id)
+        .then(data => {
+            if(data){
+                res.status(200).json(data)
+            } else {
+                res.status(404).json({
+                    message: "The user with the specified ID does not exist."
+                })
+            }       
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: "There was an error while saving the user to the database"
+            })
+        })
+});
+
 app.listen(process.env.PORT || 3000, ()=>{
     console.log('server listening on ' + (process.env.PORT || 3000))
 });
