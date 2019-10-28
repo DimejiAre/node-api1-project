29a2f1033412b68daff86c578b2e93be7340c1e7
DimejiAre <olardi2@gmail.com>
2019-10-28 13:44:23 +0100

    add endpoint for posting users

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const db = require('./data/db');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/api/users', (req, res) => {
     db.find()
         .then(data => {
@@ -39,6 +41,27 @@ app.get('/api/users/:id', (req, res) => {
             })
         })
 });
+
+app.post('/api/users', (req,res) => {
+    const user = req.body;
+
+    if(user.name && user.bio){
+        db.insert(user)
+        .then(data => {
+            res.status(201).json(data)
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: "There was an error while saving the user to the database"
+            })
+        })
+    } else {
+        res.status(400).json({
+            errorMessage: "Please provide name and bio for the user."
+        })
+    }
+   
+})
 
 app.listen(process.env.PORT || 3000, ()=>{
     console.log('server listening on ' + (process.env.PORT || 3000))
